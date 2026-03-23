34e41b4fd51cad6f6449b9e15a6cd05fb07c0a8e
admin-supreme <ayushroy18102010@gmail.com>
2026-03-23 13:50:31 +0530

    Add files via upload

---
+++ src/providers/anime/index.ts
@@ -0,0 +1,29 @@
+import Gogoanime from './gogoanime';
+import NineAnime from './9anime';
+import AnimePahe from './animepahe';
+import Zoro from './zoro';
+import AnimeFox from './animefox';
+import AnimeDrive from './animedrive';
+import Anify from './anify';
+import Crunchyroll from './crunchyroll';
+import Bilibili from './bilibili';
+import Marin from './marin';
+import AnimeSaturn from './animesaturn';
+import AnimeUnity from './animeunity';
+import MonosChinos from './monoschinos';
+
+export default {
+  Gogoanime,
+  NineAnime,
+  AnimePahe,
+  Zoro,
+  AnimeFox,
+  AnimeDrive,
+  Anify,
+  Crunchyroll,
+  Bilibili,
+  Marin,
+  AnimeSaturn,
+  AnimeUnity,
+  MonosChinos,
+};
